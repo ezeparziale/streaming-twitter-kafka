2f8f77881f8f665fd684382735e8c1235e3ef54e
Ezequiel Parziale <eparziale@gmail.com>
2022-11-22 18:52:22 -0300

    :zap: improve: new config

--- new_topic.py
+++ new_topic.py
@@ -1,20 +1,21 @@
 from confluent_kafka.admin import AdminClient, NewTopic
-import config
+
+from config import settings
 
 n_repicas = 1
 n_partitions = 3
 
 admin_client = AdminClient({
-    "bootstrap.servers": config.SERVER_KAFKA
+    "bootstrap.servers": settings.SERVER_KAFKA
 })
 
 topic_list = []
-topic_list.append(NewTopic(config.TOPIC_NAME, n_partitions, n_repicas))
+topic_list.append(NewTopic(settings.TOPIC_NAME, n_partitions, n_repicas))
 fs = admin_client.create_topics(topic_list)
 
 for topic, f in fs.items():
     try:
         f.result()
-        print("Topic {} creado".format(topic))
+        print("Topic {} created".format(topic))
     except Exception as e:
-        print("Error al crear el topic {}: {}".format(topic, e))
+        print("Error creating topic {}: {}".format(topic, e))
--- producer.py
+++ producer.py
@@ -1,12 +1,13 @@
 from tweepy import Stream
 from kafka import KafkaProducer
-import config
 
-producer = KafkaProducer(bootstrap_servers=config.SERVER_KAFKA)
+from config import settings
+
+producer = KafkaProducer(bootstrap_servers=settings.SERVER_KAFKA)
 
 class StdOutListener(Stream):
   def on_data(self, data):
-    producer.send(config.TOPIC_NAME, data)
+    producer.send(settings.TOPIC_NAME, data)
     print(data)
     return True
 
@@ -14,11 +15,7 @@ class StdOutListener(Stream):
     print(status)
   
 if __name__ == '__main__':
-  stream = StdOutListener(config.API_KEY, config.API_SECRET_KEY, config.ACCESS_TOKEN, config.ACCESS_TOKEN_SECRET )
+  stream = StdOutListener(settings.TWITTER_API_KEY, 
+  settings.TWITTER_API_SECRET_KEY, settings.TWITTER_ACCESS_TOKEN, settings.TWITTER_ACCESS_TOKEN_SECRET )
 
-  # Setting para la busqueda
-  tracks = config.TRACKS        # Palabras, usuarios, hastags a buscar
-  location = config.LOCATION    # Ubicacion area de tuits
-  languages = config.LANGUAGES  # Idioma de los tuis
-
-  stream.filter(track=tracks, locations=location, languages=languages)
+  stream.filter(track=settings.TRACKS, locations=settings.LOCATION, languages=settings.LANGUAGES)
